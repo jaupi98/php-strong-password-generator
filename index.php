4ba672ccb89605b2769f800d7bbbf27398b2e917
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-07-06 23:24:46 +0200

    millestone 1

--- index.php
+++ index.php
@@ -1,16 +1,49 @@
 <?php
 
-function generatePassword($length){
-    $numbers ='0123456789';
-    $letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY';
-    $symbols='!@#$%^&*()';
+//FUNZIOONE CHE GENERA UN NMERO CASUALE COMPRESO TRA 0 ED IL NUMERO INSERITO
+function randomNumber($min,$max){
+    return rand($min,$max);
 }
 
 
+//FUNZIONE CHE GENERA LA PASSWORD
 
-if(isset($_GET['$length'])){
+function generatePassword($length){
+    $result = ''; //VARIABILE CONTENETE I MESSAGI DI ERRORE
+    $password = ''; //VARIABILE CONTENTE LA PASSWORD GENERATA
+    $numbers ='0123456789';
+    $letters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY';
+    $symbols ='!@#$%^&*()';
+    //CASUALMENTE DEVE PRENDERE I DATI DA TUTTE E TRE LE STINGHE 
+    
+    $baseString = $numbers.$letters.$symbols;
+    //verifico la validita della lunghezza inserita
+    if(empty($length)){
+        $result = 'Nessun parametro valido';
+    }
+    else if($length < 8 || $length > 32) {
+        $result = 'Attenzione! La lunghezza della password non ripetta le condizionis';
+    }
+    else {
+        //ciclo finche la mia password non e generata ed ha la lunghezza stabilita
+        while (strlen($password) < $length) {
+            $index = randomNumber(0, strlen($baseString));//genero un indice casuale copreso tra 0 e la lunghezza di baseString
+            $char = $baseString[$index];// vado a recuperare il carattere in quel indice da quella stringa
+            $password.=$char; // concateno la password
+
+            // var_dump($password);
+        }
+        return $password;
+    }
+    return $result;
+
+}
+
+
+if(isset($_GET['length'])){
     $response = generatePassword($_GET['length']);
 }
+
 ?>
 
 
@@ -26,6 +59,11 @@ if(isset($_GET['$length'])){
     <div class="container mt-5">
         <div class="row">
             <div class="col-12 col-md-6 offset-md-3">
+                <?php if(isset($response)){ ?>
+                    <div class="alert alert-info">
+                        <?php echo $response; ?>
+                    </div>
+                <?php } ?>
                 <form action="index.php" method="GET">
                     <div class="row">
                         <div class="col-12 col-md-6">
--- readme.md
+++ readme.md
@@ -4,5 +4,9 @@ Milestone 1 Creare un form che invii in GET la lunghezza della password. Una nos
 2- collegare al file index.php
 3- creare uan funzione che prenda in input la lunghezza inserita, generi la pasword
     3.1-creare tre variabili che contengono rispettivamnete lettere , numeri  e caratteri speciali 
-    3.2-creare una variabile per il risultto finale (pasword) eduna per contenere eve
+    3.2-creare una variabile per il risultto finale (pasword) ed una per contenere eventuali errori
+    3.3 verifico il valore inserito
+    3.4 ? SE non rispecchia i criteri (stringa vuota o lunghezza non compresa tra alcuni valori) valorizza la variabile contenente  il messagio di errore 
+    3.5 : ALTRIMENTI genero la password 
+    3.5.1 -
 4-restituisca la pasword nella pagina 
