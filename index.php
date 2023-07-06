8891b6adeb81ada0ac08b5fce67864f1f837bdc8
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-07-06 16:47:35 +0200

    imposto lesercizio

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+function generatePassword($length){
+    $numbers ='0123456789';
+    $letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY';
+    $symbols='!@#$%^&*()';
+}
+
+
+
+if(isset($_GET['$length'])){
+    $response = generatePassword($_GET['length']);
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                <form action="index.php" method="GET">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <label for="length" class="control-label">Lunghezza</label>
+                            <input type="number" id="length" name="length" placeholder="Lunghezza" class="form-control">
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <button type="submit" class="btn btn-success float-end mt-3">invia</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,8 @@
+Milestone 1 Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. Scriviamo tutto (logica e layout) in un unico file index.php
+
+1-creare graficamnete la form
+2- collegare al file index.php
+3- creare uan funzione che prenda in input la lunghezza inserita, generi la pasword
+    3.1-creare tre variabili che contengono rispettivamnete lettere , numeri  e caratteri speciali 
+    3.2-creare una variabile per il risultto finale (pasword) eduna per contenere eve
+4-restituisca la pasword nella pagina 
